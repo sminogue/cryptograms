374d0e33fe7cd4c06ff8d0a294547fe076b853c6
Seamus <sminogue@yahoo.com>
2012-09-14 20:57:05 -0400

    added puzzle tray.

--- cryptograms/root/index.php
+++ cryptograms/root/index.php
@@ -4,6 +4,7 @@
 <script src="js/less-1.3.0.min.js" type="text/javascript"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.1/jquery.min.js" type="text/javascript"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0.beta6/handlebars.min.js" type="text/javascript"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js" type="text/javascript"></script>
 <script src="js/handlebars-remote-template.js" type="text/javascript"></script>
 <script src="js/cryptogram.js" type="text/javascript"></script>
 
@@ -11,6 +12,15 @@
 <body>
 
 <div class="puzzleFrame">
+<div style="margin-left:auto; 
+	    	margin-right:auto; width: 640px;">
+	<div class="puzzle"></div>
+	<div class="letter-tray" >
+		<div class="letter-tray-title" >Available Letters</div>
+		<div id="available-letters"></div>
+	</div>
+	<div style="clear: both;"></div>
+</div>
 </div>	
 
 </body>
--- cryptograms/root/js/cryptogram.js
+++ cryptograms/root/js/cryptogram.js
@@ -32,15 +32,60 @@ var cipherLines = {"cipherLines":[ [ {
 	"symbol" : "r"
 } ] ]};
 
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+//var alphabet = ['a','b','c','d','e','f'];
 //
 // Function called when the document is loaded and ready.
 //
 jQuery(document).ready(function() {
 
-	Handlebars.renderFromRemote('/handlebars/puzzle.handlebars', cipherLines, '.puzzleFrame');
+	Handlebars.renderFromRemote('handlebars/puzzle.handlebars', cipherLines, '.puzzle');
+	
+	updateLetterTray();
 	
 	jQuery('.letterbox').keyup(function() {
 		var textField = jQuery(this);
 		jQuery('.sym' + textField.attr('symbol')).val(textField.val());
+		updateLetterTray();
 	});
 });
+
+
+function updateLetterTray(){
+	
+	var col = 0;
+	var rows = [];
+	var row = [];
+	
+	var availableLetters = _.uniq(alphabet);
+	
+	jQuery('.letterbox').each(function(){
+	
+		var textField = jQuery(this);
+		
+		availableLetters = _.without(availableLetters,textField.val());
+		
+	});
+	
+	availableLetters = _.sortBy(availableLetters,function(letter){return letter;});
+	
+	_.each(availableLetters,function(letter){ 
+		if(col>0 && col % 5 == 0){
+			rows.push(row);
+			row = [];
+			row.push(letter);
+			col = 1;
+		}else{
+			row.push(letter);
+			col++;
+		}
+	});
+	
+	rows.push(row);
+	
+	var model = {"model": rows };
+	Handlebars.renderFromRemote('handlebars/letters.handlebars', model, '#available-letters');
+	
+}
+
+
